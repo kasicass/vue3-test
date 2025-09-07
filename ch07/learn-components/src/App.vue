aa597186b56f4574acedc02e44cb1952926f28e5
kasicass <kasicass@gmail.com>
2025-09-07 10:21:49 +0800

    CounterOperation

--- ch07/learn-components/src/App.vue
+++ ch07/learn-components/src/App.vue
@@ -3,6 +3,7 @@ import Header from './Header.vue'
 import Main from './Main.vue'
 import Footer from './Footer.vue'
 import NoPropAttribute from './NoPropAttribute.vue'
+import CounterOperation from './CounterOperation.vue'
 
 export default {
   components: {
@@ -10,6 +11,24 @@ export default {
     Main,
     Footer,
     NoPropAttribute,
+    CounterOperation,
+  },
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    addOne() {
+      this.counter++
+    },
+    subOne() {
+      this.counter--
+    },
+    addNNum(num, name, age) {
+      console.log(name, age)
+      this.counter += num
+    },
   },
 }
 </script>
@@ -19,6 +38,10 @@ export default {
   <Main></Main>
   <Footer></Footer>
   <NoPropAttribute class="why" id="coder" name="phay"></NoPropAttribute>
+  <div>
+    <h4>当前计数：{{ counter }}</h4>
+    <CounterOperation @add="addOne" @sub="subOne" @addN="addNNum"></CounterOperation>
+  </div>
 </template>
 
 <style scoped>
---
+++ ch07/learn-components/src/CounterOperation.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+    <input type="text" v-model.number="num"></input>
+    <button @click="incrementN">+n</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // emits: ['add', 'sub', 'addN'],
+  emits: {
+    add: null,
+    sub: null,
+    addN: (num, name, age) => {
+        if (num > 10) {
+            return true
+        }
+
+        return false
+    }
+  },
+  data() {
+    return {
+        num: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.$emit('add')
+    },
+    decrement() {
+      this.$emit('sub')
+    },
+    incrementN() {
+        this.$emit('addN', this.num, "why", 18)
+    }
+  },
+}
+</script>
